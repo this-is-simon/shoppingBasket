d6290de0c2403603f0ebb774721462f15c1d9451
Simon Atkins <this-is-simon@github.com>
2018-05-13 15:26:16 +0100

    Added shopping basket class

--- src/main/java/ShoppingBasket.java
+++ src/main/java/ShoppingBasket.java
@@ -1,2 +1,22 @@
+import java.util.ArrayList;
+
 public class ShoppingBasket {
+
+    public ArrayList<Item> items;
+    public double basketTotal;
+
+    public ShoppingBasket(){
+        this.items = new ArrayList<>();
+        this.basketTotal = 0.0;
+    }
+
+    public double getBasketTotal(){
+        return this.basketTotal;
+    }
+
+    public void addItemToBasket(Item item){
+        items.add(item);
+    }
+
+
 }
