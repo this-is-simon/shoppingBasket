2f2751e30aa8669383bedc728035be6ef443d997
Simon Atkins <this-is-simon@github.com>
2018-05-13 14:48:09 +0100

    Added Test keyword to Milk tests

--- src/test/java/MilkTest.java
+++ src/test/java/MilkTest.java
@@ -13,14 +13,17 @@ public class MilkTest {
         milk1 = new Milk("1L SuperCow Milk", "SuperCow", 1.20);
     }
 
+    @Test
     public void hasName(){
         assertEquals("1L SuperCow Milk", milk1.getName());
     }
 
+    @Test
     public void hasBrand(){
         assertEquals("SuperCow", milk1.getBrand());
     }
 
+    @Test
     public void hasCost(){
         assertEquals(1.20, milk1.getPrice(), 0.0);
     }
